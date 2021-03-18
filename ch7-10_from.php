78acd3dd5e21a869f3950d34cb0c650879f9630a
PUNTIRA SORNBONTHONG <52103347+Puntira@users.noreply.github.com>
2021-03-18 11:33:48 +0700

    Add files via upload

---
+++ ch7-10_from.php
@@ -0,0 +1,11 @@
+<html>
+<head><title> Upload file </title></head>
+<body>
+    <form enctype = "multipart/form-data" method = "post"
+        action = "ch10-7_upload.php">
+        <input type = "hidden" name = "MAX_FILE_SIZE" value = "500000">
+        เลือกไฟล์ที่ต้องการ : <input type = "file" name = "ImageFile" size = "80">
+        <input type = "submit" name = "Submit" value = "Upload Now">
+    </form>
+</body>
+</html>
